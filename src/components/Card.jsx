285993942a418d1a2cabfb0e05670807449858ca
mac <mac@macs-MacBook-Air-NatusyaZira.local>
2023-06-25 15:22:50 +0300

    Created one card

---
+++ src/components/Card.jsx
@@ -0,0 +1,69 @@
+import {  Box,
+    Card,
+    CardBody,
+    Heading,
+    CardHeader,
+    Stack,
+    StackDivider,
+    Text } from '@chakra-ui/react';
+
+
+const CreateCard = ({ data})=> {
+    const {
+        maxEstimatedDiameter,
+        numberOfPotentiallyHazardousNEOs,
+        fastestNEO,
+        closestNEO,
+        date
+    } = data;
+    
+    return (
+        
+        <Card 
+            >
+            <CardHeader> <Heading className='heading' as='h4' size='xs'>{date}</Heading>
+            </CardHeader>
+            <CardBody >
+            <Stack divider={<StackDivider />} spacing='-12'>
+                <Box>
+                    <Heading as='h5' size='xs'>
+                       {maxEstimatedDiameter} km
+                    </Heading>
+                    
+                     <Text className='text' fontSize='xs'>
+                    max estimated diameter of NEO in kilometers per day
+                    </Text>
+                    
+                </Box>
+                <Box>
+                    <Heading as='h5' size='xs'>
+                    {numberOfPotentiallyHazardousNEOs} pcs
+                    </Heading>
+                    <Text fontSize='xs'>
+                    number of potentially hazardous NEOs per day
+                    </Text>
+                </Box>
+                 <Box>
+                    <Heading as='h5' size='xs'>
+                     {closestNEO} km
+                    </Heading>
+                    <Text fontSize='xs'>
+                    closest NEO
+                    </Text>
+                </Box>
+                <Box>
+                     <Heading as='h5' size='xs'>
+                     {fastestNEO} kph
+                    </Heading>
+                    <Text fontSize='xs'>
+                    fastest NEO
+                    </Text>
+                </Box>
+                </Stack>
+                </CardBody>
+                </Card>
+                
+    )
+}
+
+export default CreateCard;
